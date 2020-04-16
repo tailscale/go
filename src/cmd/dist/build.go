3cf8a47acf9c01ad3cd3e25b925b4b2de47b950a
Brad Fitzpatrick <brad@danga.com>
2020-04-15 22:35:13 -0700

    cmd/dist: stamp git commit into runtime.Version value
    Temporary fix until golang/go#37475 is done.

--- src/cmd/dist/build.go
+++ src/cmd/dist/build.go
@@ -372,6 +372,9 @@ func findgoversion() string {
 					b = "builder " + hostType
 				}
 			}
+			if isGitRepo() {
+				b += "-ts" + chomp(run(goroot, CheckExit, "git", "log", "-n", "1", "--format=format:%h", "HEAD"))
+			}
 			return b
 		}
 	}
