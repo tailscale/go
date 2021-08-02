ae043bb6fb0327fb7bd6a28fec0466ca1ea21340
Josh Bleecher Snyder <josh@tailscale.com>
2021-08-02 16:29:50 -0700

    [tailscale1.16] crypto/elliptic: pull in most of the Go 1.17 implementation
    Because Filippo made P521 fast. Thanks, Filippo!
    
    This doesn't include CL 315189, which increases the size of the
    binary by 88kb. See the discussion on that CL for details.
    
    Change-Id: Ib16b79f34a57b28474ec12d60a95c8de1f49fb15

--- src/go/build/deps_test.go
+++ src/go/build/deps_test.go
@@ -383,6 +383,9 @@ var depsRules = `
 	< crypto
 	< crypto/subtle
 	< crypto/internal/subtle
+	< crypto/elliptic/internal/fiat
+	< crypto/ed25519/internal/edwards25519/field
+	< crypto/ed25519/internal/edwards25519
 	< crypto/cipher
 	< crypto/aes, crypto/des, crypto/hmac, crypto/md5, crypto/rc4,
 	  crypto/sha1, crypto/sha256, crypto/sha512
@@ -394,7 +397,6 @@ var depsRules = `
 	CRYPTO, FMT, math/big
 	< crypto/rand
 	< crypto/internal/randutil
-	< crypto/ed25519/internal/edwards25519
 	< crypto/ed25519
 	< encoding/asn1
 	< golang.org/x/crypto/cryptobyte/asn1
