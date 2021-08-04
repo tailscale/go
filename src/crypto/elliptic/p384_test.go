tailscale/go

commit 473e4671f2110fdd7f7982defdb5bb6beee69362

[tailscale1.16] crypto/elliptic: pull in P384 implementation from cloudflare
In the least graceful way possible. Smash the code in
until it compiles. And runs, too.

From https://github.com/cloudflare/circl

Change-Id: I1d544f8ee065476f30817bc950246c148cf50234

src/crypto/elliptic/p384_generic.go
@@ -1,0 +1,19 @@
+// +build noasm !amd64,!arm64
+
+package elliptic
+
+import (
+	"math/big"
+)
+
+type curve struct{ Curve }
+
+func P384() Curve { return curve{P384()} }
+
+// CombinedMult calculates P=mG+nQ, where G is the generator and Q=(x,y,z).
+// The scalars m and n are integers in big-endian form. Non-constant time.
+func (c curve) CombinedMult(xQ, yQ *big.Int, m, n []byte) (xP, yP *big.Int) {
+	x1, y1 := c.ScalarBaseMult(m)
+	x2, y2 := c.ScalarMult(xQ, yQ, n)
+	return c.Add(x1, y1, x2, y2)
+}

src/crypto/elliptic/p384_test.go
@@ -1,0 +1,261 @@
+package elliptic_test
+
+import (
+	"crypto/elliptic"
+	"crypto/rand"
+	"fmt"
+	"math/big"
+	"testing"
+)
+
+func TestIsOnCurveTrue(t *testing.T) {
+	CirclCurve := elliptic.P384()
+	k := make([]byte, 384/8)
+	for i := 0; i < 128; i++ {
+		_, _ = rand.Read(k)
+		x, y := elliptic.P384().ScalarBaseMult(k)
+
+		got := CirclCurve.IsOnCurve(x, y)
+		want := true
+		if got != want {
+			ReportError(t, got, want, k)
+		}
+
+		x = x.Neg(x)
+		got = CirclCurve.IsOnCurve(x, y)
+		want = false
+		if got != want {
+			ReportError(t, got, want, k)
+		}
+	}
+}
+
+func TestAffine(t *testing.T) {
+	const testTimes = 1 << 7
+	CirclCurve := elliptic.P384()
+	StdCurve := elliptic.P384()
+	params := StdCurve.Params()
+
+	t.Run("Addition", func(t *testing.T) {
+		for i := 0; i < testTimes; i++ {
+			K1, _ := rand.Int(rand.Reader, params.N)
+			K2, _ := rand.Int(rand.Reader, params.N)
+			X1, Y1 := StdCurve.ScalarBaseMult(K1.Bytes())
+			X2, Y2 := StdCurve.ScalarBaseMult(K2.Bytes())
+			wantX, wantY := StdCurve.Add(X1, Y1, X2, Y2)
+			gotX, gotY := CirclCurve.Add(X1, Y1, X2, Y2)
+
+			if gotX.Cmp(wantX) != 0 {
+				ReportError(t, gotX, wantX, K1, K2)
+			}
+			if gotY.Cmp(wantY) != 0 {
+				ReportError(t, gotY, wantY)
+			}
+		}
+	})
+
+	t.Run("Double", func(t *testing.T) {
+		for i := 0; i < testTimes; i++ {
+			k, _ := rand.Int(rand.Reader, params.N)
+			x, y := StdCurve.ScalarBaseMult(k.Bytes())
+			wantX, wantY := StdCurve.Double(x, y)
+
+			gotX, gotY := CirclCurve.Double(x, y)
+
+			if gotX.Cmp(wantX) != 0 {
+				ReportError(t, gotX, wantX, k)
+			}
+			if gotY.Cmp(wantY) != 0 {
+				ReportError(t, gotY, wantY)
+			}
+		}
+	})
+}
+
+func TestScalarBaseMult(t *testing.T) {
+	const testTimes = 1 << 6
+	CirclCurve := elliptic.P384()
+	StdCurve := elliptic.P384()
+
+	t.Run("0P", func(t *testing.T) {
+		k := make([]byte, 500)
+		for i := 0; i < len(k); i += 20 {
+			gotX, gotY := CirclCurve.ScalarBaseMult(k[:i])
+			wantX, wantY := StdCurve.ScalarBaseMult(k[:i])
+			if gotX.Cmp(wantX) != 0 {
+				ReportError(t, gotX, wantX, k[:i])
+			}
+			if gotY.Cmp(wantY) != 0 {
+				ReportError(t, gotY, wantY)
+			}
+		}
+	})
+
+	t.Run("kP", func(t *testing.T) {
+		k := make([]byte, 48)
+		for i := 0; i < testTimes; i++ {
+			_, _ = rand.Read(k)
+			gotX, gotY := CirclCurve.ScalarBaseMult(k)
+			wantX, wantY := StdCurve.ScalarBaseMult(k)
+			if gotX.Cmp(wantX) != 0 {
+				ReportError(t, gotX, wantX, k)
+			}
+			if gotY.Cmp(wantY) != 0 {
+				ReportError(t, gotY, wantY)
+			}
+		}
+	})
+
+	t.Run("kSmall", func(t *testing.T) {
+		k := make([]byte, 16)
+		for i := 0; i < testTimes; i++ {
+			_, _ = rand.Read(k)
+			gotX, gotY := CirclCurve.ScalarBaseMult(k)
+			wantX, wantY := StdCurve.ScalarBaseMult(k)
+			if gotX.Cmp(wantX) != 0 {
+				ReportError(t, gotX, wantX, k)
+			}
+			if gotY.Cmp(wantY) != 0 {
+				ReportError(t, gotY, wantY)
+			}
+		}
+	})
+
+	t.Run("kLarge", func(t *testing.T) {
+		k := make([]byte, 384)
+		for i := 0; i < testTimes; i++ {
+			_, _ = rand.Read(k)
+			gotX, gotY := CirclCurve.ScalarBaseMult(k)
+			wantX, wantY := StdCurve.ScalarBaseMult(k)
+			if gotX.Cmp(wantX) != 0 {
+				ReportError(t, gotX, wantX, k)
+			}
+			if gotY.Cmp(wantY) != 0 {
+				ReportError(t, gotY, wantY)
+			}
+		}
+	})
+}
+
+type IsAtInfinityIer interface {
+	elliptic.Curve
+	IsAtInfinity(X, Y *big.Int) bool
+	CombinedMult(Qx, Qy *big.Int, m, n []byte) (Px, Py *big.Int)
+}
+
+func TestScalarMult(t *testing.T) {
+	const testTimes = 1 << 6
+	CirclCurve := elliptic.P384().(IsAtInfinityIer)
+	StdCurve := elliptic.P384()
+	params := StdCurve.Params()
+
+	t.Run("k=0", func(t *testing.T) {
+		k := []byte{0x0}
+		gotX, gotY := CirclCurve.ScalarMult(params.Gx, params.Gy, k)
+		got := CirclCurve.IsAtInfinity(gotX, gotY)
+		want := true
+		if got != want {
+			ReportError(t, got, want)
+		}
+	})
+
+	t.Run("random k", func(t *testing.T) {
+		for i := 0; i < testTimes; i++ {
+			k, _ := rand.Int(rand.Reader, params.N)
+			gotX, gotY := CirclCurve.ScalarMult(params.Gx, params.Gy, k.Bytes())
+			wantX, wantY := StdCurve.ScalarMult(params.Gx, params.Gy, k.Bytes())
+
+			if gotX.Cmp(wantX) != 0 {
+				ReportError(t, gotX, wantX, k)
+			}
+			if gotY.Cmp(wantY) != 0 {
+				ReportError(t, gotY, wantY)
+			}
+		}
+	})
+
+	t.Run("wrong P", func(t *testing.T) {
+		for i := 0; i < testTimes; i++ {
+			k, _ := rand.Int(rand.Reader, params.N)
+			x, _ := rand.Int(rand.Reader, params.P)
+			y, _ := rand.Int(rand.Reader, params.P)
+
+			got := CirclCurve.IsOnCurve(CirclCurve.ScalarMult(x, y, k.Bytes()))
+			want := StdCurve.IsOnCurve(StdCurve.ScalarMult(x, y, k.Bytes()))
+
+			if got != want {
+				ReportError(t, got, want, k, x, y)
+			}
+		}
+	})
+}
+
+func TestCombinedMult(t *testing.T) {
+	const testTimes = 1 << 7
+	CirclCurve := elliptic.P384().(IsAtInfinityIer)
+	StdCurve := elliptic.P384()
+	params := StdCurve.Params()
+
+	for i := 0; i < testTimes; i++ {
+		K, _ := rand.Int(rand.Reader, params.N)
+		X, Y := StdCurve.ScalarBaseMult(K.Bytes())
+
+		K1, _ := rand.Int(rand.Reader, params.N)
+		K2, _ := rand.Int(rand.Reader, params.N)
+		x1, y1 := StdCurve.ScalarBaseMult(K1.Bytes())
+		x2, y2 := StdCurve.ScalarMult(X, Y, K2.Bytes())
+		wantX, wantY := StdCurve.Add(x1, y1, x2, y2)
+
+		gotX, gotY := CirclCurve.CombinedMult(X, Y, K1.Bytes(), K2.Bytes())
+		if gotX.Cmp(wantX) != 0 {
+			ReportError(t, gotX, wantX, K, K1, K2)
+		}
+		if gotY.Cmp(wantY) != 0 {
+			ReportError(t, gotY, wantY)
+		}
+	}
+}
+
+func BenchmarkScalarMult(b *testing.B) {
+	curve := elliptic.P384().(IsAtInfinityIer)
+	params := curve.Params()
+
+	K, _ := rand.Int(rand.Reader, params.N)
+	M, _ := rand.Int(rand.Reader, params.N)
+	N, _ := rand.Int(rand.Reader, params.N)
+	k := K.Bytes()
+	m := M.Bytes()
+	n := N.Bytes()
+
+	b.Run("kG", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			curve.ScalarBaseMult(k)
+		}
+	})
+	b.Run("kP", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			curve.ScalarMult(params.Gx, params.Gy, k)
+		}
+	})
+	b.Run("kG+lP", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			_, _ = curve.CombinedMult(params.Gx, params.Gy, m, n)
+		}
+	})
+}
+
+func Example_p384() {
+	// import "github.com/cloudflare/circl/ecc/p384"
+	// import "crypto/elliptic"
+	circl := elliptic.P384()
+	stdlib := elliptic.P384()
+
+	params := circl.Params()
+	K, _ := rand.Int(rand.Reader, params.N)
+	k := K.Bytes()
+
+	x1, y1 := circl.ScalarBaseMult(k)
+	x2, y2 := stdlib.ScalarBaseMult(k)
+	fmt.Printf("%v, %v", x1.Cmp(x2) == 0, y1.Cmp(y2) == 0)
+	// Output: true, true
+}

src/crypto/elliptic/LICENSE
@@ -1,0 +1,26 @@
+Copyright (c) 2018, Brendan McMillion. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors
+may be used to endorse or promote products derived from this software without
+specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.

src/crypto/elliptic/arith.go
@@ -1,0 +1,146 @@
+// +build !noasm,arm64 !noasm,amd64
+
+package elliptic
+
+import (
+	"math/big"
+)
+
+const sizeFp = 48
+
+type fp384 [sizeFp]byte
+
+func (e fp384) BigInt() *big.Int { return BytesLe2BigInt(e[:]) }
+func (e fp384) String() string   { return BytesLe2Hex(e[:]) }
+
+func (e *fp384) SetBigInt(b *big.Int) {
+	if b.BitLen() > 384 || b.Sign() < 0 {
+		b = new(big.Int).Mod(b, p.BigInt())
+	}
+	BigInt2BytesLe(e[:], b)
+}
+
+func montEncode(c, a *fp384) { fp384Mul(c, a, &r2) }
+func montDecode(c, a *fp384) { fp384Mul(c, a, &fp384{1}) }
+func fp384Sqr(c, a *fp384)   { fp384Mul(c, a, a) }
+
+func fp384Inv(z, x *fp384) {
+	t0, t1, t2, t3, t4 := &fp384{}, &fp384{}, &fp384{}, &fp384{}, &fp384{}
+	/* alpha_1 */
+	fp384Sqr(t4, x)
+	/* alpha_2 */
+	fp384Mul(t4, t4, x)
+	/* alpha_3 */
+	fp384Sqr(t0, t4)
+	fp384Mul(t0, t0, x)
+	/* alpha_6 */
+	fp384Sqr(t1, t0)
+	fp384Sqr(t1, t1)
+	fp384Sqr(t1, t1)
+	fp384Mul(t1, t1, t0)
+	/* alpha_12 */
+	fp384Sqr(t2, t1)
+	for i := 0; i < 5; i++ {
+		fp384Sqr(t2, t2)
+	}
+	fp384Mul(t2, t2, t1)
+	/* alpha_15 */
+	for i := 0; i < 3; i++ {
+		fp384Sqr(t2, t2)
+	}
+	fp384Mul(t2, t2, t0)
+	/* alpha_30 */
+	fp384Sqr(t1, t2)
+	for i := 0; i < 14; i++ {
+		fp384Sqr(t1, t1)
+	}
+	fp384Mul(t1, t1, t2)
+	/* alpha_60 */
+	fp384Sqr(t3, t1)
+	for i := 0; i < 29; i++ {
+		fp384Sqr(t3, t3)
+	}
+	fp384Mul(t3, t3, t1)
+	/* T_3 = alpha_30^(2^2) */
+	fp384Sqr(t1, t1)
+	fp384Sqr(t1, t1)
+	/* alpha_32 */
+	*t0 = *t1
+	fp384Mul(t0, t0, t4)
+	/* T_3 = a^(2^32-3) = (alpha_30)^(2^2)*alpha_1 */
+	fp384Mul(t1, t1, x)
+	/* alpha_120 */
+	fp384Sqr(t4, t3)
+	for i := 0; i < 59; i++ {
+		fp384Sqr(t4, t4)
+	}
+	fp384Mul(t4, t4, t3)
+	/* alpha_240 */
+	fp384Sqr(t3, t4)
+	for i := 0; i < 119; i++ {
+		fp384Sqr(t3, t3)
+	}
+	fp384Mul(t3, t3, t4)
+	/* alpha_255 */
+	for i := 0; i < 15; i++ {
+		fp384Sqr(t3, t3)
+	}
+	fp384Mul(t3, t3, t2)
+	/* T_5 = a^(2^288-2^32-1) = (alpha_255)^(2^33)*alpha_32 */
+	for i := 0; i < 33; i++ {
+		fp384Sqr(t3, t3)
+	}
+	fp384Mul(t3, t3, t0)
+	/* T_1 = a^(2^384-2^128-2^96+2^32-3) = (T_1)^(2^96)*T_3 */
+	fp384Sqr(t4, t3)
+	for i := 0; i < 95; i++ {
+		fp384Sqr(t4, t4)
+	}
+	fp384Mul(z, t4, t1)
+}
+
+//go:noescape
+func fp384Cmov(x, y *fp384, b int)
+
+//go:noescape
+func fp384Neg(c, a *fp384)
+
+//go:noescape
+func fp384Add(c, a, b *fp384)
+
+//go:noescape
+func fp384Sub(c, a, b *fp384)
+
+//go:noescape
+func fp384Mul(c, a, b *fp384)
+
+var (
+	// p is the order of the base field, represented as little-endian 64-bit words.
+	p = fp384{
+		0xff, 0xff, 0xff, 0xff, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0xff, 0xff, 0xff, 0xff, 0xfe, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+		0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+		0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+	}
+	// pp satisfies r*rp - p*pp = 1 where rp and pp are both integers.
+	pp = fp384{ //nolint
+		0x01, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0xfe, 0xff, 0xff, 0xff, 0xfb, 0xff, 0xff, 0xff,
+		0xfa, 0xff, 0xff, 0xff, 0xfc, 0xff, 0xff, 0xff, 0x02, 0x00, 0x00, 0x00,
+		0x0c, 0x00, 0x00, 0x00, 0x14, 0x00, 0x00, 0x00, 0x14, 0x00, 0x00, 0x00,
+	}
+	// r2 is R^2 where R = 2^384 mod p.
+	r2 = fp384{
+		0x01, 0x00, 0x00, 0x00, 0xfe, 0xff, 0xff, 0xff, 0x00, 0x00, 0x00, 0x00,
+		0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xfe, 0xff, 0xff, 0xff,
+		0x00, 0x00, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+	}
+	// bb is the Montgomery encoding of the curve parameter B.
+	bb = fp384{
+		0xcc, 0x2d, 0x41, 0x9d, 0x71, 0x88, 0x11, 0x08, 0xec, 0x32, 0x4c, 0x7a,
+		0xd8, 0xad, 0x29, 0xf7, 0x2e, 0x02, 0x20, 0x19, 0x9b, 0x20, 0xf2, 0x77,
+		0xe2, 0x8a, 0x93, 0x94, 0xee, 0x4b, 0x37, 0xe3, 0x94, 0x20, 0x02, 0x1f,
+		0xf4, 0x21, 0x2b, 0xb6, 0xf9, 0xbf, 0x4f, 0x60, 0x4b, 0x11, 0x08, 0xcd,
+	}
+)

src/crypto/elliptic/arith_amd64.go
@@ -1,0 +1,7 @@
+// +build amd64,!noasm
+
+package elliptic
+
+import "golang.org/x/sys/cpu"
+
+var hasBMI2 = cpu.X86.HasBMI2 //nolint

src/crypto/elliptic/arith_amd64.s
@@ -1,0 +1,745 @@
+// +build amd64,!noasm
+
+#include "textflag.h"
+
+#define storeBlock(a0,a1,a2,a3,a4,a5, r) \
+	MOVQ a0,  0+r \
+	MOVQ a1,  8+r \
+	MOVQ a2, 16+r \
+	MOVQ a3, 24+r \
+	MOVQ a4, 32+r \
+	MOVQ a5, 40+r
+
+#define loadBlock(r, a0,a1,a2,a3,a4,a5) \
+	MOVQ  0+r, a0 \
+	MOVQ  8+r, a1 \
+	MOVQ 16+r, a2 \
+	MOVQ 24+r, a3 \
+	MOVQ 32+r, a4 \
+	MOVQ 40+r, a5
+
+#define fp384Carry(a0,a1,a2,a3,a4,a5,a6, b0,b1,b2,b3,b4,b5,b6) \
+	\ // b = a-p
+	MOVQ a0, b0 \
+	MOVQ a1, b1 \
+	MOVQ a2, b2 \
+	MOVQ a3, b3 \
+	MOVQ a4, b4 \
+	MOVQ a5, b5 \
+	MOVQ a6, b6 \
+	\
+	SUBQ ·p+0(SB), b0 \
+	SBBQ ·p+8(SB), b1 \
+	SBBQ ·p+16(SB), b2 \
+	SBBQ ·p+24(SB), b3 \
+	SBBQ ·p+32(SB), b4 \
+	SBBQ ·p+40(SB), b5 \
+	SBBQ $0, b6 \
+	\
+	\ // if b is negative then return a
+	\ // else return b
+	CMOVQCC b0, a0 \
+	CMOVQCC b1, a1 \
+	CMOVQCC b2, a2 \
+	CMOVQCC b3, a3 \
+	CMOVQCC b4, a4 \
+	CMOVQCC b5, a5
+
+#define mul(a0,a1,a2,a3,a4,a5, rb, stack) \
+	\ // a0
+	MOVQ a0, AX \
+	MULQ 0+rb \
+	MOVQ AX, R8 \
+	MOVQ DX, R9 \
+	MOVQ a0, AX \
+	MULQ 8+rb \
+	ADDQ AX, R9 \
+	ADCQ $0, DX \
+	MOVQ DX, R10 \
+	MOVQ a0, AX \
+	MULQ 16+rb \
+	ADDQ AX, R10 \
+	ADCQ $0, DX \
+	MOVQ DX, R11 \
+	MOVQ a0, AX \
+	MULQ 24+rb \
+	ADDQ AX, R11 \
+	ADCQ $0, DX \
+	MOVQ DX, R12 \
+	MOVQ a0, AX \
+	MULQ 32+rb \
+	ADDQ AX, R12 \
+	ADCQ $0, DX \
+	MOVQ DX, R13 \
+	MOVQ a0, AX \
+	MULQ 40+rb \
+	ADDQ AX, R13 \
+	ADCQ $0, DX \
+	MOVQ DX, R14 \
+	\
+	storeBlock(R8,R9,R10,R11,R12,R13, 0+stack) \
+	MOVQ R14, 48+stack \
+	\
+	\ // a1
+	MOVQ a1, AX \
+	MULQ 0+rb \
+	MOVQ AX, R8 \
+	MOVQ DX, R9 \
+	MOVQ a1, AX \
+	MULQ 8+rb \
+	ADDQ AX, R9 \
+	ADCQ $0, DX \
+	MOVQ DX, R10 \
+	MOVQ a1, AX \
+	MULQ 16+rb \
+	ADDQ AX, R10 \
+	ADCQ $0, DX \
+	MOVQ DX, R11 \
+	MOVQ a1, AX \
+	MULQ 24+rb \
+	ADDQ AX, R11 \
+	ADCQ $0, DX \
+	MOVQ DX, R12 \
+	MOVQ a1, AX \
+	MULQ 32+rb \
+	ADDQ AX, R12 \
+	ADCQ $0, DX \
+	MOVQ DX, R13 \
+	MOVQ a1, AX \
+	MULQ 40+rb \
+	ADDQ AX, R13 \
+	ADCQ $0, DX \
+	MOVQ DX, R14 \
+	\
+	ADDQ 8+stack, R8 \
+	ADCQ 16+stack, R9 \
+	ADCQ 24+stack, R10 \
+	ADCQ 32+stack, R11 \
+	ADCQ 40+stack, R12 \
+	ADCQ 48+stack, R13 \
+	ADCQ $0, R14 \
+	storeBlock(R8,R9,R10,R11,R12,R13, 8+stack) \
+	MOVQ R14, 56+stack \
+	\
+	\ // a2
+	MOVQ a2, AX \
+	MULQ 0+rb \
+	MOVQ AX, R8 \
+	MOVQ DX, R9 \
+	MOVQ a2, AX \
+	MULQ 8+rb \
+	ADDQ AX, R9 \
+	ADCQ $0, DX \
+	MOVQ DX, R10 \
+	MOVQ a2, AX \
+	MULQ 16+rb \
+	ADDQ AX, R10 \
+	ADCQ $0, DX \
+	MOVQ DX, R11 \
+	MOVQ a2, AX \
+	MULQ 24+rb \
+	ADDQ AX, R11 \
+	ADCQ $0, DX \
+	MOVQ DX, R12 \
+	MOVQ a2, AX \
+	MULQ 32+rb \
+	ADDQ AX, R12 \
+	ADCQ $0, DX \
+	MOVQ DX, R13 \
+	MOVQ a2, AX \
+	MULQ 40+rb \
+	ADDQ AX, R13 \
+	ADCQ $0, DX \
+	MOVQ DX, R14 \
+	\
+	ADDQ 16+stack, R8 \
+	ADCQ 24+stack, R9 \
+	ADCQ 32+stack, R10 \
+	ADCQ 40+stack, R11 \
+	ADCQ 48+stack, R12 \
+	ADCQ 56+stack, R13 \
+	ADCQ $0, R14 \
+	storeBlock(R8,R9,R10,R11,R12,R13, 16+stack) \
+	MOVQ R14, 64+stack \
+	\
+	\ // a3
+	MOVQ a3, AX \
+	MULQ 0+rb \
+	MOVQ AX, R8 \
+	MOVQ DX, R9 \
+	MOVQ a3, AX \
+	MULQ 8+rb \
+	ADDQ AX, R9 \
+	ADCQ $0, DX \
+	MOVQ DX, R10 \
+	MOVQ a3, AX \
+	MULQ 16+rb \
+	ADDQ AX, R10 \
+	ADCQ $0, DX \
+	MOVQ DX, R11 \
+	MOVQ a3, AX \
+	MULQ 24+rb \
+	ADDQ AX, R11 \
+	ADCQ $0, DX \
+	MOVQ DX, R12 \
+	MOVQ a3, AX \
+	MULQ 32+rb \
+	ADDQ AX, R12 \
+	ADCQ $0, DX \
+	MOVQ DX, R13 \
+	MOVQ a3, AX \
+	MULQ 40+rb \
+	ADDQ AX, R13 \
+	ADCQ $0, DX \
+	MOVQ DX, R14 \
+	\
+	ADDQ 24+stack, R8 \
+	ADCQ 32+stack, R9 \
+	ADCQ 40+stack, R10 \
+	ADCQ 48+stack, R11 \
+	ADCQ 56+stack, R12 \
+	ADCQ 64+stack, R13 \
+	ADCQ $0, R14 \
+	storeBlock(R8,R9,R10,R11,R12,R13, 24+stack) \
+	MOVQ R14, 72+stack \
+	\
+	\ // a4
+	MOVQ a4, AX \
+	MULQ 0+rb \
+	MOVQ AX, R8 \
+	MOVQ DX, R9 \
+	MOVQ a4, AX \
+	MULQ 8+rb \
+	ADDQ AX, R9 \
+	ADCQ $0, DX \
+	MOVQ DX, R10 \
+	MOVQ a4, AX \
+	MULQ 16+rb \
+	ADDQ AX, R10 \
+	ADCQ $0, DX \
+	MOVQ DX, R11 \
+	MOVQ a4, AX \
+	MULQ 24+rb \
+	ADDQ AX, R11 \
+	ADCQ $0, DX \
+	MOVQ DX, R12 \
+	MOVQ a4, AX \
+	MULQ 32+rb \
+	ADDQ AX, R12 \
+	ADCQ $0, DX \
+	MOVQ DX, R13 \
+	MOVQ a4, AX \
+	MULQ 40+rb \
+	ADDQ AX, R13 \
+	ADCQ $0, DX \
+	MOVQ DX, R14 \
+	\
+	ADDQ 32+stack, R8 \
+	ADCQ 40+stack, R9 \
+	ADCQ 48+stack, R10 \
+	ADCQ 56+stack, R11 \
+	ADCQ 64+stack, R12 \
+	ADCQ 72+stack, R13 \
+	ADCQ $0, R14 \
+	storeBlock(R8,R9,R10,R11,R12,R13, 32+stack) \
+	MOVQ R14, 80+stack \
+	\
+	\ // a5
+	MOVQ a5, AX \
+	MULQ 0+rb \
+	MOVQ AX, R8 \
+	MOVQ DX, R9 \
+	MOVQ a5, AX \
+	MULQ 8+rb \
+	ADDQ AX, R9 \
+	ADCQ $0, DX \
+	MOVQ DX, R10 \
+	MOVQ a5, AX \
+	MULQ 16+rb \
+	ADDQ AX, R10 \
+	ADCQ $0, DX \
+	MOVQ DX, R11 \
+	MOVQ a5, AX \
+	MULQ 24+rb \
+	ADDQ AX, R11 \
+	ADCQ $0, DX \
+	MOVQ DX, R12 \
+	MOVQ a5, AX \
+	MULQ 32+rb \
+	ADDQ AX, R12 \
+	ADCQ $0, DX \
+	MOVQ DX, R13 \
+	MOVQ a5, AX \
+	MULQ 40+rb \
+	ADDQ AX, R13 \
+	ADCQ $0, DX \
+	MOVQ DX, R14 \
+	\
+	ADDQ 40+stack, R8 \
+	ADCQ 48+stack, R9 \
+	ADCQ 56+stack, R10 \
+	ADCQ 64+stack, R11 \
+	ADCQ 72+stack, R12 \
+	ADCQ 80+stack, R13 \
+	ADCQ $0, R14 \
+	storeBlock(R8,R9,R10,R11,R12,R13, 40+stack) \
+	MOVQ R14, 88+stack
+
+#define fp384Reduce(stack) \
+	\ // m = (T * P') mod R, store m in R8:R9:R10:R11:R12:R13
+	MOVQ ·pp+0(SB), AX \
+	MULQ 0+stack \
+	MOVQ AX, R8 \
+	MOVQ DX, R9 \
+	MOVQ ·pp+0(SB), AX \
+	MULQ 8+stack \
+	ADDQ AX, R9 \
+	ADCQ $0, DX \
+	MOVQ DX, R10 \
+	MOVQ ·pp+0(SB), AX \
+	MULQ 16+stack \
+	ADDQ AX, R10 \
+	ADCQ $0, DX \
+	MOVQ DX, R11 \
+	MOVQ ·pp+0(SB), AX \
+	MULQ 24+stack \
+	ADDQ AX, R11 \
+	ADCQ $0, DX \
+	MOVQ DX, R12 \
+	MOVQ ·pp+0(SB), AX \
+	MULQ 32+stack \
+	ADDQ AX, R12 \
+	ADCQ $0, DX \
+	MOVQ DX, R13 \
+	MOVQ ·pp+0(SB), AX \
+	MULQ 40+stack \
+	ADDQ AX, R13 \
+	\
+	ADDQ 0+stack, R9 \
+	ADCQ 8+stack, R10 \
+	ADCQ 16+stack, R11 \
+	ADCQ 24+stack, R12 \
+	ADCQ 32+stack, R13 \
+	\
+	MOVQ ·pp+16(SB), AX \
+	MULQ 0+stack \
+	MOVQ AX, R14 \
+	MOVQ DX, R15 \
+	MOVQ ·pp+16(SB), AX \
+	MULQ 8+stack \
+	ADDQ AX, R15 \
+	ADCQ $0, DX \
+	MOVQ DX, BX \
+	MOVQ ·pp+16(SB), AX \
+	MULQ 16+stack \
+	ADDQ AX, BX \
+	ADCQ $0, DX \
+	MOVQ DX, CX \
+	MOVQ ·pp+16(SB), AX \
+	MULQ 24+stack \
+	ADDQ AX, CX \
+	\
+	ADDQ R14, R10 \
+	ADCQ R15, R11 \
+	ADCQ BX, R12 \
+	ADCQ CX, R13 \
+	\
+	MOVQ ·pp+24(SB), AX \
+	MULQ 0+stack \
+	MOVQ AX, R14 \
+	MOVQ DX, R15 \
+	MOVQ ·pp+24(SB), AX \
+	MULQ 8+stack \
+	ADDQ AX, R15 \
+	ADCQ $0, DX \
+	MOVQ DX, BX \
+	MOVQ ·pp+24(SB), AX \
+	MULQ 16+stack \
+	ADDQ AX, BX \
+	\
+	ADDQ R14, R11 \
+	ADCQ R15, R12 \
+	ADCQ BX, R13 \
+	\
+	MOVQ ·pp+32(SB), AX \
+	MULQ 0+stack \
+	MOVQ AX, R14 \
+	MOVQ DX, R15 \
+	MOVQ ·pp+32(SB), AX \
+	MULQ 8+stack \
+	ADDQ AX, R15 \
+	\
+	ADDQ R14, R12 \
+	ADCQ R15, R13 \
+	\
+	MOVQ ·pp+40(SB), AX \
+	MULQ 0+stack \
+	ADDQ AX, R13 \
+	\
+	storeBlock(R8,R9,R10,R11,R12,R13, 96+stack) \
+	\
+	\ // m * P
+	mul(·p+0(SB),·p+8(SB),·p+16(SB),·p+24(SB),·p+32(SB),·p+40(SB), 96+stack, 144+stack) \
+	\
+	\ // Add the 768-bit intermediate to m*N
+	MOVQ $0, DI \
+	loadBlock(144+stack, R8,R9,R10,R11,R12,R13) \
+	loadBlock(192+stack, R14,R15,AX,BX,CX,DX) \
+	\
+	ADDQ 0+stack, R8 \
+	ADCQ 8+stack, R9 \
+	ADCQ 16+stack, R10 \
+	ADCQ 24+stack, R11 \
+	ADCQ 32+stack, R12 \
+	ADCQ 40+stack, R13 \
+	ADCQ 48+stack, R14 \
+	ADCQ 56+stack, R15 \
+	ADCQ 64+stack, AX \
+	ADCQ 72+stack, BX \
+	ADCQ 80+stack, CX \
+	ADCQ 88+stack, DX \
+	ADCQ $0, DI \
+	\
+	fp384Carry(R14,R15,AX,BX,CX,DX,DI, R8,R9,R10,R11,R12,R13,SI)
+
+#define mulBMI2(a0,a1,a2,a3,a4,a5, rb, stack) \
+	MOVQ a0, DX \
+	MULXQ 0+rb, R8, R9 \
+	MULXQ 8+rb, AX, R10 \
+	ADDQ AX, R9 \
+	MULXQ 16+rb, AX, R11 \
+	ADCQ AX, R10 \
+	MULXQ 24+rb, AX, R12 \
+	ADCQ AX, R11 \
+	MULXQ 32+rb, AX, R13 \
+	ADCQ AX, R12 \
+	MULXQ 40+rb, AX, R14 \
+	ADCQ AX, R13 \
+	ADCQ $0, R14 \
+	\
+	MOVQ R8, 0+stack \
+	MOVQ $0, R15 \
+	MOVQ $0, R8 \
+	\
+	MOVQ a1, DX \
+	MULXQ 0+rb, AX, BX \
+	ADDQ AX, R9 \
+	ADCQ BX, R10 \
+	MULXQ 16+rb, AX, BX \
+	ADCQ AX, R11 \
+	ADCQ BX, R12 \
+	MULXQ 32+rb, AX, BX \
+	ADCQ AX, R13 \
+	ADCQ BX, R14 \
+	ADCQ $0, R15 \
+	MULXQ 8+rb, AX, BX \
+	ADDQ AX, R10 \
+	ADCQ BX, R11 \
+	MULXQ 24+rb, AX, BX \
+	ADCQ AX, R12 \
+	ADCQ BX, R13 \
+	MULXQ 40+rb, AX, BX \
+	ADCQ AX, R14 \
+	ADCQ BX, R15 \
+	ADCQ $0, R8 \
+	\
+	MOVQ R9, 8+stack \
+	MOVQ $0, R9 \
+	\
+	MOVQ a2, DX \
+	MULXQ 0+rb, AX, BX \
+	ADDQ AX, R10 \
+	ADCQ BX, R11 \
+	MULXQ 16+rb, AX, BX \
+	ADCQ AX, R12 \
+	ADCQ BX, R13 \
+	MULXQ 32+rb, AX, BX \
+	ADCQ AX, R14 \
+	ADCQ BX, R15 \
+	ADCQ $0, R8 \
+	MULXQ 8+rb, AX, BX \
+	ADDQ AX, R11 \
+	ADCQ BX, R12 \
+	MULXQ 24+rb, AX, BX \
+	ADCQ AX, R13 \
+	ADCQ BX, R14 \
+	MULXQ 40+rb, AX, BX \
+	ADCQ AX, R15 \
+	ADCQ BX, R8 \
+	ADCQ $0, R9 \
+	\
+	MOVQ R10, 16+stack \
+	MOVQ $0, R10 \
+	\
+	MOVQ a3, DX \
+	MULXQ 0+rb, AX, BX \
+	ADDQ AX, R11 \
+	ADCQ BX, R12 \
+	MULXQ 16+rb, AX, BX \
+	ADCQ AX, R13 \
+	ADCQ BX, R14 \
+	MULXQ 32+rb, AX, BX \
+	ADCQ AX, R15 \
+	ADCQ BX, R8 \
+	ADCQ $0, R9 \
+	MULXQ 8+rb, AX, BX \
+	ADDQ AX, R12 \
+	ADCQ BX, R13 \
+	MULXQ 24+rb, AX, BX \
+	ADCQ AX, R14 \
+	ADCQ BX, R15 \
+	MULXQ 40+rb, AX, BX \
+	ADCQ AX, R8 \
+	ADCQ BX, R9 \
+	ADCQ $0, R10 \
+	\
+	MOVQ R11, 24+stack \
+	MOVQ $0, R11 \
+	\
+	MOVQ a4, DX \
+	MULXQ 0+rb, AX, BX \
+	ADDQ AX, R12 \
+	ADCQ BX, R13 \
+	MULXQ 16+rb, AX, BX \
+	ADCQ AX, R14 \
+	ADCQ BX, R15 \
+	MULXQ 32+rb, AX, BX \
+	ADCQ AX, R8 \
+	ADCQ BX, R9 \
+	ADCQ $0, R10 \
+	MULXQ 8+rb, AX, BX \
+	ADDQ AX, R13 \
+	ADCQ BX, R14 \
+	MULXQ 24+rb, AX, BX \
+	ADCQ AX, R15 \
+	ADCQ BX, R8 \
+	MULXQ 40+rb, AX, BX \
+	ADCQ AX, R9 \
+	ADCQ BX, R10 \
+	ADCQ $0, R11 \
+	\
+	MOVQ R12, 32+stack \
+	\
+	MOVQ a5, DX \
+	MULXQ 0+rb, AX, BX \
+	ADDQ AX, R13 \
+	ADCQ BX, R14 \
+	MULXQ 16+rb, AX, BX \
+	ADCQ AX, R15 \
+	ADCQ BX, R8 \
+	MULXQ 32+rb, AX, BX \
+	ADCQ AX, R9 \
+	ADCQ BX, R10 \
+	ADCQ $0, R11 \
+	MULXQ 8+rb, AX, BX \
+	ADDQ AX, R14 \
+	ADCQ BX, R15 \
+	MULXQ 24+rb, AX, BX \
+	ADCQ AX, R8 \
+	ADCQ BX, R9 \
+	MULXQ 40+rb, AX, BX \
+	ADCQ AX, R10 \
+	ADCQ BX, R11 \
+	\
+	MOVQ R13, 40+stack
+
+#define fp384ReduceBMI2(stack) \
+	\ // m = (T * P') mod R, store m in R8:R9:R10:R11:R12:R13
+	MOVQ ·pp+0(SB), DX \
+	MULXQ 0+stack, R8, R9 \
+	MULXQ 8+stack, AX, R10 \
+	ADDQ AX, R9 \
+	MULXQ 16+stack, AX, R11 \
+	ADCQ AX, R10 \
+	MULXQ 24+stack, AX, R12 \
+	ADCQ AX, R11 \
+	MULXQ 32+stack, AX, R13 \
+	ADCQ AX, R12 \
+	MULXQ 40+stack, AX, BX \
+	ADCQ AX, R13 \
+	\
+	ADDQ 0+stack, R9 \
+	ADCQ 8+stack, R10 \
+	ADCQ 16+stack, R11 \
+	ADCQ 24+stack, R12 \
+	ADCQ 32+stack, R13 \
+	\
+	MOVQ ·pp+16(SB), DX \
+	MULXQ 0+stack, AX, BX \
+	ADDQ AX, R10 \
+	ADCQ BX, R11 \
+	MULXQ 16+stack, AX, BX \
+	ADCQ AX, R12 \
+	ADCQ BX, R13 \
+	MULXQ 8+stack, AX, BX \
+	ADDQ AX, R11 \
+	ADCQ BX, R12 \
+	MULXQ 24+stack, AX, BX \
+	ADCQ AX, R13 \
+	\
+	MOVQ ·pp+24(SB), DX \
+	MULXQ 0+stack, AX, BX \
+	ADDQ AX, R11 \
+	ADCQ BX, R12 \
+	MULXQ 16+stack, AX, BX \
+	ADCQ AX, R13 \
+	MULXQ 8+stack, AX, BX \
+	ADDQ AX, R12 \
+	ADCQ BX, R13 \
+	\
+	MOVQ ·pp+32(SB), DX \
+	MULXQ 0+stack, AX, BX \
+	ADDQ AX, R12 \
+	ADCQ BX, R13 \
+	MULXQ 8+stack, AX, BX \
+	ADDQ AX, R13 \
+	\
+	MOVQ ·pp+40(SB), DX \
+	MULXQ 0+stack, AX, BX \
+	ADDQ AX, R13 \
+	\
+	storeBlock(R8,R9,R10,R11,R12,R13, 96+stack) \
+	\
+	\ // m * P
+	mulBMI2(·p+0(SB),·p+8(SB),·p+16(SB),·p+24(SB),·p+32(SB),·p+40(SB), 96+stack, 144+stack) \
+	\
+	\ // Add the 768-bit intermediate to m*N
+	MOVQ $0, AX \
+	loadBlock(144+stack, R12,R13,BX,CX,DX,DI) \
+	\
+	ADDQ 0+stack, R12 \
+	ADCQ 8+stack, R13 \
+	ADCQ 16+stack, BX \
+	ADCQ 24+stack, CX \
+	ADCQ 32+stack, DX \
+	ADCQ 40+stack, DI \
+	ADCQ 48+stack, R14 \
+	ADCQ 56+stack, R15 \
+	ADCQ 64+stack, R8 \
+	ADCQ 72+stack, R9 \
+	ADCQ 80+stack, R10 \
+	ADCQ 88+stack, R11 \
+	ADCQ $0, AX \
+	\
+	fp384Carry(R14,R15,R8,R9,R10,R11,AX, R12,R13,BX,CX,DX,DI,SI)
+
+TEXT ·fp384Neg(SB), NOSPLIT, $0-16
+	MOVQ ·p+0(SB), R8
+	MOVQ ·p+8(SB), R9
+	MOVQ ·p+16(SB), R10
+	MOVQ ·p+24(SB), R11
+	MOVQ ·p+32(SB), R12
+	MOVQ ·p+40(SB), R13
+
+	MOVQ a+8(FP), DI
+	SUBQ 0(DI), R8
+	SBBQ 8(DI), R9
+	SBBQ 16(DI), R10
+	SBBQ 24(DI), R11
+	SBBQ 32(DI), R12
+	SBBQ 40(DI), R13
+
+	MOVQ $0, R14
+	fp384Carry(R8,R9,R10,R11,R12,R13,R14, R15,AX,BX,CX,DX,DI,SI)
+
+	MOVQ c+0(FP), DI
+	storeBlock(R8,R9,R10,R11,R12,R13, 0(DI))
+	RET
+
+TEXT ·fp384Add(SB), NOSPLIT, $0-24
+	MOVQ a+8(FP), DI
+	MOVQ b+16(FP), SI
+
+	loadBlock(0(DI), R8,R9,R10,R11,R12,R13)
+	MOVQ $0, R14
+
+	ADDQ  0(SI), R8
+	ADCQ  8(SI), R9
+	ADCQ 16(SI), R10
+	ADCQ 24(SI), R11
+	ADCQ 32(SI), R12
+	ADCQ 40(SI), R13
+	ADCQ $0, R14
+
+	fp384Carry(R8,R9,R10,R11,R12,R13,R14, R15,AX,BX,CX,DX,DI,SI)
+
+	MOVQ c+0(FP), DI
+	storeBlock(R8,R9,R10,R11,R12,R13, 0(DI))
+	RET
+
+TEXT ·fp384Sub(SB), NOSPLIT, $0-24
+	MOVQ ·p+0(SB), R8
+	MOVQ ·p+8(SB), R9
+	MOVQ ·p+16(SB), R10
+	MOVQ ·p+24(SB), R11
+	MOVQ ·p+32(SB), R12
+	MOVQ ·p+40(SB), R13
+
+	MOVQ b+16(FP), DI
+	SUBQ 0(DI), R8
+	SBBQ 8(DI), R9
+	SBBQ 16(DI), R10
+	SBBQ 24(DI), R11
+	SBBQ 32(DI), R12
+	SBBQ 40(DI), R13
+
+	MOVQ $0, R14
+	MOVQ a+8(FP), DI
+	ADDQ 0(DI), R8
+	ADCQ 8(DI), R9
+	ADCQ 16(DI), R10
+	ADCQ 24(DI), R11
+	ADCQ 32(DI), R12
+	ADCQ 40(DI), R13
+	ADCQ $0, R14
+
+	fp384Carry(R8,R9,R10,R11,R12,R13,R14, R15,AX,BX,CX,DX,DI,SI)
+
+	MOVQ c+0(FP), DI
+	storeBlock(R8,R9,R10,R11,R12,R13, 0(DI))
+	RET
+
+TEXT ·fp384Mul(SB), NOSPLIT, $240-24
+	MOVQ a+8(FP), DI
+	MOVQ b+16(FP), SI
+
+	// Jump to a slightly different implementation if MULX isn't supported.
+	CMPB ·hasBMI2(SB), $0
+	JE   nobmi2Mul
+
+	// T = a * b
+	mulBMI2(0(DI),8(DI),16(DI),24(DI),32(DI),40(DI), 0(SI), 0(SP))
+	storeBlock(R14,R15,R8,R9,R10,R11, 48(SP))
+
+	// Reduce T.
+	fp384ReduceBMI2(0(SP))
+
+	MOVQ c+0(FP), DI
+	storeBlock(R14,R15,R8,R9,R10,R11, 0(DI))
+	JMP end
+
+nobmi2Mul:
+	// T = a * b
+	mul(0(DI),8(DI),16(DI),24(DI),32(DI),40(DI), 0(SI), 0(SP))
+
+	// Reduce T.
+	fp384Reduce(0(SP))
+
+	MOVQ c+0(FP), DI
+	storeBlock(R14,R15,AX,BX,CX,DX, 0(DI))
+
+end:
+	RET
+
+TEXT ·fp384Cmov(SB), NOSPLIT, $0
+    MOVQ x+0(FP), DI
+    MOVQ y+8(FP), SI
+    MOVQ b+16(FP), BX
+    TESTQ BX, BX
+    MOVQ  0(DI), AX; MOVQ  0(SI), DX; CMOVQNE DX, AX; MOVQ AX,  0(DI);
+    MOVQ  8(DI), AX; MOVQ  8(SI), DX; CMOVQNE DX, AX; MOVQ AX,  8(DI);
+    MOVQ 16(DI), AX; MOVQ 16(SI), DX; CMOVQNE DX, AX; MOVQ AX, 16(DI);
+    MOVQ 24(DI), AX; MOVQ 24(SI), DX; CMOVQNE DX, AX; MOVQ AX, 24(DI);
+    MOVQ 32(DI), AX; MOVQ 32(SI), DX; CMOVQNE DX, AX; MOVQ AX, 32(DI);
+    MOVQ 40(DI), AX; MOVQ 40(SI), DX; CMOVQNE DX, AX; MOVQ AX, 40(DI);
+    RET

commit edbc3e19a064177abc6efe937cb84c2a261053ab

[tailscale1.16] crypto/elliptic: fix P384 implementation on !arm64,!amd64
Fixes an infinite loop introduced in 473e4671f2110fdd7f7982defdb5bb6beee69362.

src/crypto/elliptic/p384_generic.go
@@ -8,7 +8,17 @@ import (
 
 type curve struct{ Curve }
 
-func P384() Curve { return curve{P384()} }
+// P384 returns a Curve which implements NIST P-384 (FIPS 186-3, section D.2.4),
+// also known as secp384r1. The CurveParams.Name of this Curve is "P-384".
+//
+// Multiple invocations of this function will return the same value, so it can
+// be used for equality checks and switch statements.
+//
+// The cryptographic operations do not use constant-time algorithms.
+func P384() Curve {
+	initonce.Do(initAll)
+	return p384
+}
 
 // CombinedMult calculates P=mG+nQ, where G is the generator and Q=(x,y,z).
 // The scalars m and n are integers in big-endian form. Non-constant time.

src/crypto/elliptic/p384_test.go
@@ -145,7 +145,10 @@ type IsAtInfinityIer interface {
 
 func TestScalarMult(t *testing.T) {
 	const testTimes = 1 << 6
-	CirclCurve := elliptic.P384().(IsAtInfinityIer)
+	CirclCurve, ok := elliptic.P384().(IsAtInfinityIer)
+	if !ok {
+		t.Skip("using generic implementation")
+	}
 	StdCurve := elliptic.P384()
 	params := StdCurve.Params()
 
@@ -192,7 +195,10 @@ func TestScalarMult(t *testing.T) {
 
 func TestCombinedMult(t *testing.T) {
 	const testTimes = 1 << 7
-	CirclCurve := elliptic.P384().(IsAtInfinityIer)
+	CirclCurve, ok := elliptic.P384().(IsAtInfinityIer)
+	if !ok {
+		t.Skip("using generic implementation")
+	}
 	StdCurve := elliptic.P384()
 	params := StdCurve.Params()
 
@@ -217,7 +223,10 @@ func TestCombinedMult(t *testing.T) {
 }
 
 func BenchmarkScalarMult(b *testing.B) {
-	curve := elliptic.P384().(IsAtInfinityIer)
+	curve, ok := elliptic.P384().(IsAtInfinityIer)
+	if !ok {
+		b.Skip("using generic implementation")
+	}
 	params := curve.Params()
 
 	K, _ := rand.Int(rand.Reader, params.N)