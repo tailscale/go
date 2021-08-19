d07e00f38aeb895e01104a8e35278c34738cc2d3
Denton Gentry <dgentry@tailscale.com>
2021-08-19 06:35:32 -0700

    Revert "internal/syscall/unix: use internal/abi.FuncPC for syscall wrapper"
    This reverts commit 731a015ab8c8f44c7196123fd65b184a63d6835a.

--- src/internal/syscall/unix/getentropy_darwin.go
+++ src/internal/syscall/unix/getentropy_darwin.go
@@ -5,7 +5,6 @@
 package unix
 
 import (
-	"internal/abi"
 	"syscall"
 	"unsafe"
 )
@@ -16,7 +15,7 @@ func libc_getentropy_trampoline()
 
 // GetEntropy calls the macOS getentropy system call.
 func GetEntropy(p []byte) error {
-	_, _, errno := syscall_syscall(abi.FuncPCABI0(libc_getentropy_trampoline),
+	_, _, errno := syscall_syscall(funcPC(libc_getentropy_trampoline),
 		uintptr(unsafe.Pointer(&p[0])),
 		uintptr(len(p)),
 		0)
@@ -28,3 +27,6 @@ func GetEntropy(p []byte) error {
 
 //go:linkname syscall_syscall syscall.syscall
 func syscall_syscall(fn, a1, a2, a3 uintptr) (r1, r2 uintptr, err syscall.Errno)
+
+//go:linkname funcPC runtime.funcPC
+func funcPC(f interface{}) uintptr
