b387e68b65d2bc37daa2fda2440c8bd834af5655
Brad Fitzpatrick <bradfitz@tailscale.com>
2025-10-17 07:53:15 -0700

    [tailscale] runtime: add TailscaleNumTimers for metrics
    Updates tailscale/go#141
    
    (cherry picked from commit 615142f0507d20846b6a5b385e15bcbc103c8c35)

--- src/runtime/tailscale_runtime.go
+++ src/runtime/tailscale_runtime.go
@@ -10,3 +10,24 @@ package runtime
 func TailscaleCurrentP() int {
 	return int(getg().m.p.ptr().id)
 }
+
+// TailscaleNumTimers returns the number of timers that are currently
+// pending in the runtime across all Ps, as well as the number of "zombie"
+// timers.
+func TailscaleNumTimers() (total, zombies int) {
+	var sum uint32
+	var nzombies int32
+
+	// Prevent allp slice changes. This is like retake.
+	lock(&allpLock)
+	for _, pp := range allp {
+		if pp == nil {
+			continue
+		}
+		sum += pp.timers.len.Load()
+		nzombies += pp.timers.zombies.Load()
+	}
+	unlock(&allpLock)
+
+	return int(sum), int(nzombies)
+}
